5985d099c70ca832546dae70222c9fa7c111c930
gustavo <gimuller@nelogica.com.br>
2024-04-16 23:05:51 -0300

    iterative deep + gbfs

--- Lista 1/src/main.cpp
+++ Lista 1/src/main.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 
 #include "solvers/solvers.cpp"
+#include "state/state8.cpp"
 
 int main(int argc, char **argv) {
-    std::vector<int> testState = {0, 6, 1, 7, 4, 2, 3, 8, 5};
+    std::vector<int> testState = {2, 4, 7, 0, 3, 6, 8, 1, 5};
 
     State8 state = State8(testState);
 
-    ret_info sol = bfs_graph<State8>(state);
+    ret_info sol = greedy_best_first<State8>(state);
 
     std::cout << "\n";
     std::cout << sol.expanded << ",";
     std::cout << sol.sol.size() - 1 << ",";
-    std::cout << sol.time << std::setprecision(20) << ",";
-    std::cout << sol.avg_h << ",";
+    std::cout << sol.time << std::setprecision(5) << ",";
+    std::cout << (float) sol.avg_h / sol.expanded << std::setprecision(5) << ",";
     std::cout << sol.start_h << "\n\n";
 
-    for (std::shared_ptr<Node> &i : sol.sol) {
+    /*for (std::shared_ptr<Node> &i : sol.sol) {
         i->print();
         std::cout << "\n";
-    }
+    }*/
 }
 
 
--- Lista 1/src/solvers/solvers.cpp
+++ Lista 1/src/solvers/solvers.cpp
@@ -1,10 +1,11 @@
 #ifndef SOLVERS_H
 #define SOLVERS_H
 
-#include "../state/state8.cpp"
-#include "../state/state16.cpp"
+#include "../state/node.cpp"
 #include <deque>
 #include <map>
+
+const int MAX_DEPTH = 1e7;
 
 struct ret_info {
     int expanded = 0;
@@ -14,6 +15,7 @@ struct ret_info {
     int avg_h = 0;
 };
 
+/** BFS GRAPH */
 template<typename State>
 ret_info bfs_graph(State state) {
     ret_info ret;
@@ -51,6 +53,122 @@ ret_info bfs_graph(State state) {
             if (!closed[next->toString()]) {
                 closed[next->toString()] = true;
                 open.push_back(next);
+            }
+        }
+    }
+
+    end = clock();
+    ret.time = double(end - start) / double(CLOCKS_PER_SEC);
+    return ret;
+}
+
+
+/** ITERATIVE DEEPENING */
+template<typename State>
+ret_info iter_deep(State state) {
+    ret_info ret;
+    clock_t start, end;
+    start = clock();
+
+    std::shared_ptr<Node> n = state.init();
+    ret.start_h = state.h_value(n);
+
+    for (int i = 0; i < MAX_DEPTH; ++i) {
+        dfs(n, &state, &ret, i);
+        if (!ret.sol.empty())
+            break;
+    }
+
+    end = clock();
+    ret.time = double(end - start) / double(CLOCKS_PER_SEC);
+    return ret;
+}
+
+template<typename State>
+void dfs(std::shared_ptr<Node> n, State *state, ret_info *ret, int depth) {
+
+    if (state->is_goal(n)) {
+        ret->sol = state->extract_path(n);
+        return;
+    }
+
+    if (depth > 0) {
+        ret->expanded += 1;
+        for (std::shared_ptr<Node> next: state->succ(n)) {
+            dfs(next, state, ret, depth - 1);
+            if (!ret->sol.empty())
+                return;
+        }
+    }
+}
+
+
+/** GREEDY BEST FIRST SEARCH */
+struct QueueNode {
+    std::shared_ptr<Node> node;
+    int h;
+    int g;
+    int pos;
+};
+
+class Compare {
+public:
+    bool operator() (QueueNode f, QueueNode s)
+    {
+        if (f.h > s.h)
+            return true;
+        if (f.h == s.h && f.g < s.g)
+            return true;
+        if (f.h == s.h && f.g == s.g && f.pos < s.pos)
+            return true;
+        return false;
+    }
+};
+
+template<typename State>
+ret_info greedy_best_first(State state) {
+    ret_info ret;
+    clock_t start, end;
+    start = clock();
+
+    std::shared_ptr<Node> n = state.init();
+    ret.start_h = state.h_value(n);
+
+    int cur_pos = 0;
+
+    std::priority_queue<QueueNode, std::deque<QueueNode>, Compare> open;
+    open.push({
+        n,
+        state.h_value(n),
+        n->path_cost,
+        ++cur_pos
+    });
+
+    std::map<std::string, bool> closed = std::map<std::string, bool>();
+
+    while (!open.empty()) {
+        QueueNode front = open.top();
+        open.pop();
+
+        if (!closed[front.node->toString()]) {
+            closed[front.node->toString()] = true;
+            if (state.is_goal(front.node)) {
+                ret.sol = state.extract_path(front.node);
+                end = clock();
+                ret.time = double(end - start) / double(CLOCKS_PER_SEC);
+                return ret;
+            }
+
+            ret.expanded += 1;
+            ret.avg_h += front.h;
+
+            for (std::shared_ptr<Node> next: state.succ(front.node)) {
+                open.push(QueueNode {
+                    next,
+                    state.h_value(next),
+                    next->path_cost,
+                    ++cur_pos
+                });
             }
         }
     }
