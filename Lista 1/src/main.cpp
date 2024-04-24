c7aaaccf700cbc63fc01ec46c3b9069221b8e171
HeitorCAndrade <hcandrade@inf.ufrgs.br>
2024-04-24 16:42:51 -0300

    finished state 16 and started idastar

--- Lista 1/src/main.cpp
+++ Lista 1/src/main.cpp
@@ -2,13 +2,17 @@
 
 #include "solvers/solvers.cpp"
 #include "state/state8.cpp"
+//#include "state/state16.cpp"
 
 int main(int argc, char **argv) {
     std::vector<int> testState = {0, 6, 1, 7, 4, 2, 3, 8, 5};
+    //std::vector<int> testState = {7, 11, 8, 3, 14, 0, 6, 15, 1, 4, 13, 9, 5, 12, 2, 10};
 
     State8 state = State8(testState);
+    //State16 state = State16(testState);
 
-    ret_info sol = astar<State8>(state);
+    ret_info sol = idastar<State8>(state);
+    //ret_info sol = astar<State16>(state);
 
     std::cout << "\n";
     std::cout << sol.expanded << ",";
